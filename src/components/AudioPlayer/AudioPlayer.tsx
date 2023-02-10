40426851570b4dd88f4c7a48ba0e95b8192934e9
gbenites <gracielabenitesf@gmail.com>
2023-02-09 21:21:23 -0500

    feat: playing audio

--- src/components/AudioPlayer/AudioPlayer.tsx
+++ src/components/AudioPlayer/AudioPlayer.tsx
@@ -1,21 +1,27 @@
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import './AudioPlayer.scss';
 
 import mp3 from '../../music/mp3-demo.mp3';
 import disk from '../../images/disk.png';
 import diskArm from '../../images/disk-arm.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCaretLeft, faCaretRight, faPlay } from '@fortawesome/free-solid-svg-icons';
+import { faCaretLeft, faCaretRight, faGripLinesVertical, faPlay, faStepBackward, faStepForward, faWallet } from '@fortawesome/free-solid-svg-icons';
 
 
 
 const AudioPlayer = () => {
 
     const musicRef = useRef<HTMLAudioElement>(null);
-    var myaudio = new Audio(mp3);
-/*     document.body.addEventListener("mousemove", function () {
-        myaudio.play()
-    }) */
+/*     const myaudio = new Audio(mp3); */
+    const [audio, setAudio] = useState( new Audio(mp3) )
+    
+    const [showPlay, setShowPlay] = useState(true)
+
+
+    function toPlayMusic() {
+        setShowPlay(!showPlay);
+        showPlay ? audio.play(): audio.pause();
+    }
     
     return (
         <div className="audioPlayer">
@@ -26,66 +32,47 @@ const AudioPlayer = () => {
                 <div className="audioPlayer__disk--img">
                     <img src={disk} alt="" />
 
-    
                 {/*      <audio id="myAudio" controls autoPlay>
                 <source src={mp3} type="audio/mp3" ></source>
                 Your browser does not support the audio element.
                 </audio> */}
 
-
                 </div>
             </div>
-          
-
-
 
             <div className="audioPlayer__buttons">
 
                 <div className="audioPlayer__buttons--left">
-                    <FontAwesomeIcon icon={faCaretLeft} /> 
-                    <FontAwesomeIcon icon={faCaretLeft} />
+                    <FontAwesomeIcon icon={faStepForward} /> 
                 </div>
 
-                <div className="audioPlayer__button--play">
+                {
+            /*     showPlay && 
+                (<div className="audioPlayer__button--play"  onClick={toPlayMusic}>
+                    <FontAwesomeIcon icon={faPlay} />
+                </div>) */
+                showPlay ? 
+                (<div className="audioPlayer__button--play" onClick={toPlayMusic}>
                     <FontAwesomeIcon icon={faPlay} />
                 </div>
-
+                ) :
+                (
+                <div className="audioPlayer__button--stop" onClick={toPlayMusic}>
+                    <FontAwesomeIcon icon={faGripLinesVertical} /> 
+                </div>
+                
+                )
+                }
+    
                 <div className="audioPlayer__buttons--right">
-                    <FontAwesomeIcon icon={faCaretRight} /> 
-                    <FontAwesomeIcon icon={faCaretRight} />
+                    <FontAwesomeIcon icon={faStepBackward} />
                 </div>
 
+            </div>
 
-                </div>
-
-
-
-         {/*        <div className="audioPlayer__buttons">
-         
-                    <div className="audioPlayer__buttons--left">
-                        <FontAwesomeIcon icon={faCaretLeft} /> 
-                        <FontAwesomeIcon icon={faCaretLeft} />
-                    </div>
-
-                    <div className="audioPlayer__button--play">
-                        <FontAwesomeIcon icon={faPlay} />
-                    </div>
-
-                    <div className="audioPlayer__buttons--right">
-                        <FontAwesomeIcon icon={faCaretRight} /> 
-                        <FontAwesomeIcon icon={faCaretRight} />
-                    </div>
-
-
-                </div> */}
-
-                <ul>
-              
-
-                  {/* <audio ref={musicRef} src={mp3} controls ></audio>  */}
-        
-                </ul>
-
+            <ul>
+                {/* <audio ref={musicRef} src={mp3} controls ></audio>  */}
+            </ul>
 
         </div>
     )
